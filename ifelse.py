34f93919e11875a8ca10c083fec75579d40aa249
sheenamsehmi <115872687+sheenamsehmi@users.noreply.github.com>
2022-10-15 14:03:04 +0100

    practice

---
+++ ifelse.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+# i = 10
+# if 10 > 15:
+#     print(" 10 is greater than 15")
+
+# else: #false
+#     print(" 10 is less than 15")    
+
+
+age = 20
+
+if age == 17:
+    print(" you are eligible for vote")
+
+else: #false
+    print(" you are not ellgible")
